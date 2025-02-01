8cc0fe794eadbd27cb12ed77f0234b42b67e0d40
PMSValerio <37459155+VileOar@users.noreply.github.com>
2025-02-01 12:47:58 +0000

    refactored styles and resume page

--- assets/js/reusable.js
+++ assets/js/reusable.js
@@ -54,7 +54,11 @@ class Footer extends HTMLElement {
 }
 
 let headContent = `
-    <!-- Favicons -->
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <!-- Favicons -->
   <link href="assets/img/favicon.png" rel="icon">
   <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
